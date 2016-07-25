1a54dc1d194de3284c9ee346d7a1fcbed405b3a6
Giles Van Gruisen <gilesvangruisen@me.com>
2016-07-25 16:30:37 -0400

    Limit multiline iOS inputs to `numberOfLines` prop
    This adds functionality to allow multiline TextInput components to pass
    the `numberOfLines` prop on iOS. If provided, input will not be allwoed
    when that line count has been reached, but will still allow input
    shorter than that line count.

--- Libraries/Components/TextInput/TextInput.js
+++ Libraries/Components/TextInput/TextInput.js
@@ -34,6 +34,7 @@ const requireNativeComponent = require('requireNativeComponent');
 const PropTypes = React.PropTypes;
 
 const onlyMultiline = {
+  numberOfLines: true,
   onTextInput: true,
   children: true,
 };
@@ -307,7 +308,6 @@ const TextInput = React.createClass({
     /**
      * Sets the number of lines for a `TextInput`. Use it with multiline set to
      * `true` to be able to fill the lines.
-     * @platform android
      */
     numberOfLines: PropTypes.number,
     /**
@@ -664,6 +664,7 @@ const TextInput = React.createClass({
           ref={this._setNativeRef}
           {...props}
           children={children}
+          numberOfLines={this.props.numberOfLines}
           onFocus={this._onFocus}
           onBlur={this._onBlur}
           onChange={this._onChange}
